078cd1f0aef43e848a7bdbe7d3d80ed8db010e56
SHREYANK-RAJ <shreyankraj2005@gmail.com>
2025-08-11 22:29:10 +0530

    check_address.cpp

---
+++ check_address.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+
+    // Prints the value of num
+    cout << "num : " << num << endl;
+
+    // Prints the address where num is stored in memory
+    cout << "Address of num : " << &num << endl;
+
+    return 0;
+}
